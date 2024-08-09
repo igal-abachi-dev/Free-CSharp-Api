56be2eff5019949dd2c37ef5ef1e70d2719e8ac9
igal abachi <61175191+igal-abachi-dev@users.noreply.github.com>
2024-08-09 03:31:32 +0300

    Create KeepAliveController.cs

---
+++ Controllers/KeepAliveController.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+[ApiController]
+[Route("[controller]")]
+public class KeepAliveController : ControllerBase
+{
+    private static long _startTimestamp = Stopwatch.GetTimestamp();
+    private static int _isHealthy = 1; 
+
+
+    public static void StartUptimeTimer()
+    {
+        //in Main() of exe: KeepAliveController.StartUptimeTimer()
+        _startTimestamp = Stopwatch.GetTimestamp();
+    }
+    public static void SetIsHealthy(bool isHealthy)
+    {
+        Interlocked.Exchange(ref _isHealthy, isHealthy ? 1 : 0);
+    }
+
+    //like default,  WebApplication.CreateBuilder(args).Services.AddHealthChecks();
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var uptime = GetElapsedTime(_startTimestamp, Stopwatch.GetTimestamp());
+        var isHealthy = Interlocked.Read(ref _isHealthy) == 1;
+        var response = new
+        {
+            status = isHealthy ? "Healthy" : "Unhealthy",
+            totalDuration = uptime.ToString("c") //"1.02:03:04"
+        };
+
+        return _isHealthy ? Ok(response) : StatusCode(503, response);
+    }
+
+    private static readonly double TimestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+    private TimeSpan GetElapsedTime(long startTimestamp, long endTimestamp)
+    {
+        var timestampDelta = endTimestamp - startTimestamp;
+        var ticks = (long)(TimestampToTicks * timestampDelta);
+        return new TimeSpan(ticks);
+    }
+}
